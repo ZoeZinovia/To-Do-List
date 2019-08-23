5c53c7ddcd754b153c3a0e6cf1a43ccb0b386758
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2019-08-23 10:06:44 +0100

    add javascript file

---
+++ Assets/js/todos.js
@@ -0,0 +1,15 @@
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+   if(event.which===13){
+       $("ul").append("<li><span>x </span>"+$(this).val()+"</li>");
+       $(this).val(""); 
+   }
+});
