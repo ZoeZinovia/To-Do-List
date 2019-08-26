22fd26c1a9edd0f407128931505c6693f9b30663
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2019-08-26 17:02:37 +0100

    add final updated to todo

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,15 +1,21 @@
 $("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
+
+$(".fa-plus").on("click", function(){
+    $("input[type='text']").fadeToggle();
+});
+
 $("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
 });
+
 $("input[type='text']").keypress(function(event){
    if(event.which===13){
-       $("ul").append("<li><span>x </span>"+$(this).val()+"</li>");
+       $("ul").append("<li><span><i class='far fa-trash-alt'></i> </span>"+$(this).val()+"</li>");
        $(this).val(""); 
    }
 });
