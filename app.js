fb033713fd0cf0abdbf0dd36d9dc4d260ce36050
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2019-08-27 07:32:30 +0100

    change index from html to ejs file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("index.html");
+    res.render("index");
 });
 
 app.listen(process.env.PORT || 3000, function(){
